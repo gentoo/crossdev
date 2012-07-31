0374b1bd4c2de5f06eb53eeaf02dcb1b659616d2
Mike Frysinger <vapier@gentoo.org>
2012-07-31 12:18:56 -0400

    utilize $EPREFIX in the env if it is set
    URL: https://bugs.gentoo.org/384167
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- settings.mk
+++ settings.mk
@@ -1,8 +1,9 @@
-# Copyright 2008-2010 Gentoo Foundation
+# Copyright 2008-2012 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
 DESTDIR ?=
-PREFIX ?= /usr
+EPREFIX ?= /usr
+PREFIX ?= $(EPREFIX)
 
 INSTALL_DIR  = install -m 755 -d
 INSTALL_EXEC = install -m 755
